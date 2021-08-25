2ad56363ee6cc08a556a3e9bdc5e7daf2098cf07
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2021-08-25 14:02:16 +0200

    stocktakes on hold

---
+++ app/js/inventory/stocktakes/stocktakesController.js
@@ -0,0 +1,42 @@
+angular.module('stocktakesController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('stocktakesController', ['$scope', '$state', '$stateParams', '$rootScope', '$mdToast', 'openmrsRest', 'toastr', function ($scope, $state, $stateParams, $rootScope, $mdToast, openmrsRest, toastr) {
+    $scope.resource = "savicspharmacy";
+    $rootScope.links = { "Pharmacy management module": "", "Inventory": "index.html#!/inventory" };
+    $scope.loading = false;
+    $scope.stocktakes = [];
+    $scope.stockAtRisOnly = false;
+
+    $scope.getData = function () {
+        $scope.loading = true;
+        openmrsRest.getFull($scope.resource + "/item").then(function (response) {
+            $scope.stocktakes = response.results;
+            $scope.loading = false;
+        }, function (e) {
+            $scope.loading = false;
+            toastr.error('An unexpected error has occured.', 'Error');
+        });
+    };
+
+    $scope.getData();
+
+    $scope.options = {
+        autoSelect: true,
+        boundaryLinks: false,
+        largeEditDialog: true,
+        pageSelector: true,
+        rowSelection: true
+    };
+
+    $scope.query = {
+        limit: 5,
+        page: 1
+    };
+
+    $scope.logPagination = function (page, limit) {
+
+    };
+
+
+
+
+
+}])
